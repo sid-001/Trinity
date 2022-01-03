62979b79a9af033a84d3212b865c8e304cacd879
Siddhartha Gaur <67626049+sid-001@users.noreply.github.com>
2022-01-03 20:57:31 +0530

    Update main.py

--- Trinity/main.py
+++ Trinity/main.py
@@ -46,11 +46,20 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if (message.content.lower().startswith('hi')):
-        await message.channel.send("Hey, I'm your dad!")
+    if (message.content.lower().startswith('hi') and message.author.id!=s_admin):
+        await message.reply("hello, I'm your dad!")
         
-    elif (message.content.lower().startswith('hello')):
-        await message.channel.send("Hey, I'm your dad!")
+    elif (message.content.lower().startswith('hello') and message.author.id!=s_admin):
+        await message.reply("Hey, I'm your dad!")
+    
+    elif (message.content.lower().startswith('hey') and message.author.id!=s_admin):
+        await message.reply("Hi, I'm your dad!")
+        
+    elif (message.content.lower().startswith('hlo') and message.author.id!=s_admin):
+        await message.reply("Hello, I'm your dad!")
+        
+    elif (message.content.lower().startswith('hemlo') and message.author.id!=s_admin):
+        await message.reply("Hola, I'm your dad!")
 
     await client.process_commands(message)
 
