0df3fa8fe6ad31d8366837b6388aa7d40ec17af9
Siddhartha Gaur <67626049+sid-001@users.noreply.github.com>
2022-01-07 11:20:20 +0530

    Update config.py

--- Trinity/config.py
+++ Trinity/config.py
@@ -1,1 +1,2 @@
-bot_token = 
+bot_token = 'ODg5MzY4NDQ2MTkyNzM0MjA5.YUgO6Q.uBYG00vvUjk4mXXAlZvrsLvGZEU'
+dbstring = "mongodb+srv://SidDB:iqYEMReHesQ0pNAJ@sidbot.81mkh.mongodb.net/retryWrites=true&w=majority"
