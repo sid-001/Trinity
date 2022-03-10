b285b812747c5f8198bce47fb4df39c51c3bbed7
Saurabh254 <sauravvishwakarma030@gmail.com>
2022-03-10 23:15:35 +0530

    making it better

--- main.py
+++ main.py
@@ -9,24 +9,28 @@ channel__id = 941355481589485630
 
 bot = commands.Bot(command_prefix="s!")
 
+# hello guys, I want to fetch a message using a given message_id under on_message. How can I do this
+
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Messages", url="https://google.com/"))
     print("bot is ready!!!")
 
+
 @bot.event
 async def on_message(message):
     if not message.author.bot:
-        msg = re.sub('```| |\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\+|\||\-|\=|\|\[|\]|\:|\;|\"|\'|\<|\>|\?|\,|\.|\{|\}|\d+|:saurabh:|:sau:','',message.content.lower())
-        
-        if "saurabh" in msg or "saur" in msg or "sarabh" in msg or 'surabh' in msg: 
+        msg = re.sub(
+            '```| |\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\+|\||\-|\=|\|\[|\]|\:|\;|\"|\'|\<|\>|\?|\,|\.|\{|\}|:saurabh:|:sau:|:saur:|\d+', '', message.content.lower())
+
+        if "saurabh" in msg or "sau" in msg or "sarabh" in msg or 'surabh' in msg:
+            ref_message = None
             reporting = bot.get_channel(channel__id)
             embed = discord.Embed(
                 title=f"Solicitor Headquarters!!!",
                 color=16718362)
-            id = await message.reference
-            print(message.channel.fetch_message(id.message_id))
+
             embed.add_field(
                 name="Message",
                 value=f"```py\n{(message.content[0:1000]).replace('```','')}\n```",
@@ -47,25 +51,52 @@ async def on_message(message):
                 value=f"```py\n💚 {round(bot.latency * 1000)}ms\n```",
                 inline=True)
 
+            if message.reference is not None:
+                reference_channel = message.channel
+                ref_message = await reference_channel.fetch_message(message.reference.message_id)
+
+                embed.add_field(
+                    name="Reference Message:",
+                    value=f"```\n{(ref_message.content).replace('```','')}\n```",
+                    inline=False,
+                )
+
+                embed.add_field(
+                    name="Reference Author:",
+                    value=f"```\n{ref_message.author}\n```",
+                    inline=True,
+                )
+
+                embed.add_field(
+                    name="Ref-Author's Nick:",
+                    value=f"```\n{ref_message.author.display_name}\n```",
+                    inline=True,
+                )
+
             embed.add_field(
                 name="Server Name",
                 value=f"```py\n{message.guild.name}\n```",
-                inline=True)
+                inline=False)
 
             embed.add_field(
                 name="Channel",
                 value=f"```py\n{message.channel}\n```",
-                inline=True)
+                inline=False)
 
             embed.add_field(
-                name="Message URL:",
+                name="Main Message URL:",
                 value=f"[Jump to message]({message.jump_url})",
-                inline=False)
+                inline=True)
+            if ref_message is not None:
+                embed.add_field(
+                    name="Ref Message URL:",
+                    value=f"[Jump to message]({ref_message.jump_url})",
+                    inline=True)
 
             embed.set_thumbnail(
                 url=str(message.guild.icon_url))
             embed.timestamp = datetime.utcnow()
-            
+
             embed.set_footer(text=f"Channel id : {message.channel.id}, Guild id: {message.guild.id}",
                              icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
 
